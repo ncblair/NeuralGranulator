1e0599fa96d7aeada2fa50dd097b7ede11be9067
Nathan Blair <nblair@berkeley.edu>
2021-10-06 01:15:36 -0700

    granulator now works with midi and has a nice interface

--- granulator.py
+++ granulator.py
@@ -1,63 +1,83 @@
 import pyaudio
 import numpy as np
+import librosa
+import pygame.midi
 
 # http://people.csail.mit.edu/hubert/pyaudio/docs/
-
+# https://github.com/khalidtouch/XigmaLessons/blob/6d95e1961ce86a8258e398f00411b37ad5bc80bf/PYTHON/Music_Player_App/pygame/tests/midi_test.py
 # https://stackoverflow.com/questions/31674416/python-realtime-audio-streaming-with-pyaudio-or-something-else
+
 
 class Granulator:
 
 	def __init__(self):
-		self.prevGrain = []
-		self.currentGrain = []
+		self.grains = {i:[0, [0], 0] for i in range(128)}
 		self.counter = 0
 	
 	def replace_grain(self, grain):
-		self.prevGrain = self.currentGrain
-		self.currentGrain = grain
-	
+		for note in self.grains:
+			self.grains[note][2] = 0
+		self.grains[60][1] = grain
+		self.grains[60][2] = 1
+
 	def init_audio_stream(self, sample_rate, bit_width):
+		self.sample_rate = sample_rate
 		self.pya = pyaudio.PyAudio()
 		self.stream = self.pya.open(format=self.pya.get_format_from_width(width=bit_width), channels=1, rate=sample_rate, output=True, stream_callback=self.callback)
 	
+	def init_midi(self):
+		pygame.midi.init()
+		in_id = pygame.midi.get_default_input_id()
+		for indx in range(pygame.midi.get_count()):
+			print(indx, pygame.midi.get_device_info(indx))
+		in_id = int(input("input number of midi device or -1 for no midi"))
+		if in_id != -1:
+			self.midi_input = pygame.midi.Input(in_id)
+			print(pygame.midi.get_device_info(in_id))
+		else:
+			self.midi_input = None
+			self.grains[60][0] = 1
+
+
 	def start_audio_stream(self):
 		self.stream.start_stream()
 
 	def close_audio_stream(self):
 		self.stream.stop_stream()
 		self.stream.close()
-		pya.terminate()
+		self.pya.terminate()
+		if self.midi_input:
+			self.midi_input.close()
+			pygame.midi.quit()
+
+	def note_on(self, note):
+		self.grains[note][0] = 1
+		if self.grains[note][2] == 0:
+			self.grains[note][1] = librosa.effects.pitch_shift(self.grains[60][1], 
+														self.sample_rate, 
+														n_steps=note - 60)
+			self.grains[note][2] = 1
+
+	def note_off(self, note):
+		self.grains[note][0] = 0
+					
 	
 	def callback(self, in_data, frame_count, time_info, status):
 		# TODO TODO TODO, make this work pureley with array operations
+		# num_repetitions = 1 + frame_count // self.grain_length
+		# data = np.repeat(self.shiftedGrain, num_repetitions).astype(np.float32)
+		# data = data[self.counter:self.counter + frame_count]
 		data = np.zeros(frame_count, dtype="float32")
-		count = 0
-		for i in range(self.counter, self.counter + frame_count):
-			data[count] = self.currentGrain[i % len(self.currentGrain)]
-			count += 1
 
-		self.counter = (self.counter + frame_count) % len(self.currentGrain)
+		grains = np.array([grain for note, (on, grain, _) in self.grains.items() if on])
+		if grains.shape[0] > 0:
+			grain = np.sum(grains, axis=0)
+			count = 0
+			for i in range(self.counter, self.counter + frame_count):
+				data[count] = grain[i % len(grain)]
+				count += 1
+
+			self.counter = (self.counter + frame_count) % len(grain)
+		
 
 		return (data, pyaudio.paContinue)
-
-
-##### Testing stuff
-# import time
-
-# SR = 16000 #khz
-# GRAIN_LEN = 1600 # samples
-# FREQ = 220
-# BIT_WIDTH = 4
-# # generate sine array
-# samples = np.arange(GRAIN_LEN)
-# sine = np.sin(2 * np.pi * FREQ * samples / SR)
-
-# # initialize granulator
-# gran = Granulator()
-# gran.replace_grain(sine)
-
-# gran.init_audio_stream(SR, BIT_WIDTH)
-# gran.start_audio_stream()
-
-# while gran.stream.is_active():
-#    time.sleep(0.1)
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
+import math
 
 import pygame
+import pygame.midi
 from sklearn.decomposition import PCA
 import numpy as np
 import nsgt
 import torch
 import soundfile as sf
 import pyaudio
+import colorsys
+from scipy.ndimage.filters import gaussian_filter
 
 from model import GrainVAE
 from granulator import Granulator
@@ -15,7 +19,8 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(PATH, "MODELS", "grain_model2.pt")
 EMBEDDINGS_PATH = os.path.join(PATH, "EMBEDDINGS", "latents.npy")
 DATA_PATH = os.path.join(PATH, "DATA", "grains.npy")
-SCREEN_SIZE = 500
+SCREEN_SIZE = 250
+WINDOW_SIZE = 750
 SCREEN_COLOR = (255, 255, 255)
 USE_CUDA = False
 SR = 16000
@@ -23,6 +28,7 @@ BIT_WIDTH = 4
 
 # Get latent data
 latent_data = np.load(EMBEDDINGS_PATH)
+latent_dim = latent_data.shape[1]
 
 # Get PCA embedding
 pca = PCA(n_components=2, whiten=True)
@@ -52,20 +58,92 @@ if USE_CUDA:
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
-screen.fill(SCREEN_COLOR)
-pygame.display.flip()
+win = pygame.display.set_mode([WINDOW_SIZE, WINDOW_SIZE])
+screen = pygame.Surface((SCREEN_SIZE, SCREEN_SIZE))
+
+circle_pos = (SCREEN_SIZE/2, SCREEN_SIZE/2)
+circle_size = SCREEN_SIZE/50
+# circle_size = circle_size_mean
+# circle_size_variance = SCREEN_SIZE / 400
+z = torch.zeros(1, latent_dim)
 
 # Init Granulator
 gran = Granulator()
 gran.replace_grain(np.zeros(grain_length))
 gran.init_audio_stream(SR, BIT_WIDTH)
 gran.start_audio_stream()
+gran.init_midi()
+
+def get_latent_vector(pos):
+	# get corresponding latent vector
+	pos = (pos * 6 / SCREEN_SIZE) - 3
+	z = pca.inverse_transform(np.expand_dims(pos, 0))
+	z = torch.Tensor(z)
+	if USE_CUDA:
+		z.cuda()
+	return z
+
+def update_audio(z):
+	# get model nsgt out
+	model_out = model.decoder(z)
+	complex_out = model.to_complex_repr(model_out)
+	nsgt_out = list(complex_out.reshape(nsgt_shape[0], nsgt_shape[1]))
+
+	# get audio from nsgt inverse transform
+	audio_out = transform.backward(nsgt_out) 
+
+	# normalize audio to help prevent clipping
+	audio_out = audio_out / np.max(np.abs(audio_out))
+	gran.replace_grain(audio_out)
+
+def draw_background_circles(z):
+	"""
+	visualize the latent space as HUE
+	z: latent vector
+	"""
+	saturation = 1
+	value = 1
+	sqrt_num_circles = int(math.sqrt(latent_dim))
+	z = z.numpy()[0]
+	min_z = np.min(z)
+	max_z = np.max(z)
+	if min_z != max_z: # avoid divide by zero
+		z = (z - min_z) / (max_z - min_z) # normalize z
+	for i in range(sqrt_num_circles):
+		for j in range(sqrt_num_circles):
+			position = (SCREEN_SIZE / 2 + np.array([i, j]) * SCREEN_SIZE)
+			position = position / (sqrt_num_circles)
+			hue = z[i*sqrt_num_circles + j]
+			color = np.array(colorsys.hsv_to_rgb(hue,saturation,value))
+			color = list((255*color).astype(np.int))
+			size = circle_size # + np.random.rand()*circle_size_variance
+			pygame.draw.circle(screen, color, position, size)
+
+def distort(surface, blur = .25):
+	imgdata = pygame.surfarray.array3d(surface)
+	# noisy = imgdata - np.random.rand(*imgdata.shape) * 50
+	# noisy = np.clip(noisy, 0, 255)
+	blurred = gaussian_filter(imgdata, sigma=(3, 3, 0))
+	distorted = pygame.surfarray.make_surface(blurred)
+
+	surface.blit(distorted, (0, 0))
+
+# Start Audio
+circle_pos = np.array([SCREEN_SIZE/2, SCREEN_SIZE/2])
+z = get_latent_vector(circle_pos)
+update_audio(z)
 
 # Run PyGame Loop
 running = True
 while running:
+	# get mouse position in [0, 1]
+	mouse_pos = np.array(pygame.mouse.get_pos()) * SCREEN_SIZE / WINDOW_SIZE
 
+	# draw
+	screen.fill(SCREEN_COLOR)
+	draw_background_circles(z)
+	distort(screen)
+	pygame.draw.circle(screen, (0,0,0), circle_pos, (SCREEN_SIZE/50))
 	# Did the user click the window close button?
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -73,21 +151,38 @@ while running:
 
 		# On Mouse Button Up
 		if event.type == pygame.MOUSEBUTTONUP:
-			# Get Corresponding Latent Vector
-			mouse_pos = -2 + np.array(pygame.mouse.get_pos())*4/SCREEN_SIZE
-			z = pca.inverse_transform(np.expand_dims(mouse_pos, 0))
-			z = torch.Tensor(z)
-			if USE_CUDA:
-				z.cuda()
+			z = get_latent_vector(circle_pos)
+			update_audio(z)
 
-			# get model nsgt out
-			model_out = model.decoder(z)
-			complex_out = model.to_complex_repr(model_out)
-			nsgt_out = list(complex_out.reshape(nsgt_shape[0], nsgt_shape[1]))
+	# If mouse being pressed
+	if pygame.mouse.get_pressed()[0]:
+		circle_pos = mouse_pos
+		z = get_latent_vector(circle_pos)
+		update_audio(z)
 
-			# get audio from nsgt inverse transform
-			audio_out = transform.backward(nsgt_out)
-			gran.replace_grain(audio_out)
+
+	# # if midi in
+	if gran.midi_input is not None and gran.midi_input.poll():
+		# if event.type == pygame.midi.MIDIIN:
+		midi_events = gran.midi_input.read(10)
+		# convert them into pygame events.
+		midi_evs = pygame.midi.midis2events(midi_events, gran.midi_input.device_id)
+
+		for m_e in midi_evs:
+			if m_e.status == 144:
+				note = m_e.data1
+				gran.note_on(note)
+
+			if m_e.status == 128:
+				note = m_e.data1
+				gran.note_off(note)
+
+
+	transformed_screen = pygame.transform.scale(screen,(WINDOW_SIZE, WINDOW_SIZE))
+	win.blit(transformed_screen, (0, 0))
+		
+	pygame.display.flip()
+
 
 gran.close_audio_stream()
 
