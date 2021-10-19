d824835ae3f1c513538b6497416db208b28264e7
Jack Kilgore <kilgorej99@gmail.com>
2021-10-19 10:18:23 -0700

    finished slider; need to draw on different screen though

--- gui.py
+++ gui.py
@@ -16,8 +16,10 @@ screen.fill(screenColor)
 pygame.display.update(pygame.Rect(0,0,800,600))
 
 class Knob :
+   
     def __init__(self,sx,sy,width,height,bg_color,fg_color,min_val, max_val,start_val):
-        self.DRAG_MODE = False
+        self.INCREMENT_RES = 64
+        self.drag_mode = False
         self.x = sx
         self.y = sy
         self.width = width
@@ -27,7 +29,7 @@ class Knob :
         self.max_val = max_val
         self.min_val = min_val
         self.cur_val = start_val
-        self.incr = (max_val - min_val) / 127
+        self.incr = (max_val - min_val) / self.INCREMENT_RES
         val_norm = (float(min_val + self.cur_val)) / (max_val - min_val)
         self.old_mouseY = -1000
 
@@ -37,23 +39,21 @@ class Knob :
         pygame.display.update(pygame.Rect(self.x,self.y,self.width,self.height))
 
     def draw(self):
-                
-        if click[0] == 0 and self.DRAG_MODE == True:
-            self.DRAG_MODE = False
+        if click[0] == 0 and self.drag_mode == True:
+            self.drag_mode = False
 
         # if mouse[0] > sx and mouse[0] < sx+width and mouse[1] > sy and mouse[1] < sy+height:
         if click[0] == 1 and mouse[0] > self.x and mouse[0] < self.x+self.width and mouse[1] > self.y and mouse[1] < self.y+self.height:
-            self.DRAG_MODE = True
+            self.drag_mode = True
         
-        if self.DRAG_MODE and click[0] == 1:
+        if self.drag_mode and click[0] == 1:
             mouseY = mouse[1]
+            mouseY_diff = mouseY - self.old_mouseY
             # print(mouseY, self.old_mouseY)
-            if mouseY < self.old_mouseY:
+            if mouseY_diff < 0:
                 self.cur_val = np.clip(self.cur_val - self.incr, self.min_val, self.max_val)
-            elif mouseY > self.old_mouseY:
+            elif mouseY_diff > 0:
                 self.cur_val = np.clip(self.cur_val + self.incr, self.min_val, self.max_val)
-            else:
-                return
 
 
             val_norm = (float(self.min_val + self.cur_val)) / (self.max_val - self.min_val)
@@ -83,4 +83,3 @@ while True:
         click = pygame.mouse.get_pressed()
     
     knob.draw()
-    # slider(100,120,"test2",1000,50,(255,255,255),(220,220,220),1,0,25,10,100)
