bc3960fb1ea622005efae3c36da713e81e13b103
Jack Kilgore <kilgorej99@gmail.com>
2021-10-27 08:46:20 -0700

    OSC send working from JS client

--- INTERFACE/server.js
+++ INTERFACE/server.js
@@ -6,15 +6,13 @@ var http = require('http'),
 // https://socket.io/get-started/chat
 
 
-console.log("To start the demo, go to http://localhost:8080 in your web browser.");
+console.log("go to http://localhost:8080 in your web browser.");
 
 const server = http.createServer((req, res) => {
-	// Why doesn't this work! This might cause OSC to fail hmm.
-	// res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080/socket.io/");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-	res.setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+	// res.setHeader("Access-Control-Allow-Origin", "*");
+	// res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+	// res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	// res.setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
 	let url = ''
 	if (req.url === '/') {
 		url = '/index.html'
@@ -33,40 +31,35 @@ const server = http.createServer((req, res) => {
 })
 
 var isConnected = false;
-// var io = require('socket.io')(server, {
-// 	cors: {
-// 	  origin: "http://localhost:8080",
-// 	  methods: ["GET", "POST"],
-// 	}
-//   });
 
 var oscServer, oscClient
 var io = socket(server)
 
 io.sockets.on('connection', function (socket) {
-	console.log('connection');
 	socket.on("config", function (obj) {
-		isConnected = true;
-    	oscServer = new osc.Server(obj.server.port, obj.server.host);
-	    oscClient = new osc.Client(obj.client.host, obj.client.port);
-	    oscClient.send('/status', socket.sessionId + ' connected');
+    	oscServer = new osc.Server(obj.server.port, obj.server.host)
+	    oscClient = new osc.Client(obj.client.host, obj.client.port)
+	    oscClient.send('/status', socket.sessionId + ' connected')
 		oscServer.on('message', function(msg, rinfo) {
-			socket.emit("message", msg);
+			socket.emit("message", msg)
 		});
-		socket.emit("connected", 1);
+		socket.emit("connected", 1)
+		isConnected = true
 	});
  	socket.on("message", function (obj) {
-		oscClient.send.apply(oscClient, obj);
+		oscClient.send.apply(oscClient, obj)
   	});
 	socket.on('disconnect', function(){
 		if (isConnected) {
-			oscServer.kill();
-			oscClient.kill();
+			oscServer.close()
+			oscClient.close()
 		}
+		isConnected = false
   	});
 });
 
 server.listen(8080)
+
 
 
 
--- osc_latent.py
+++ osc_latent.py
@@ -8,22 +8,37 @@ from pythonosc import osc_server
 from config import IP, PORT
 
 #IP = "192.168.1.207" # eudoram"169.231.37.250"
-IP = "localhost"
+IP = "127.0.0.1"
 PORT = 57121
 
+LATENT_2D_ADDR = "/1/latent_xy"
+ATTACK_ADDR = "1/attack"
+DECAY_ADDR = "1/decay"
+SUSTAIN_ADDR = "1/sustain"
+RELEASE_ADDR = "1/release"
+
 class OSCLatent:
-	data = np.zeros(2)
+	def __init__(self):
+		latent_2d = np.zeros(2)
+		attack
+
 	def get_coordinates_handler(self,*args):
 		address = args[0]
-		self.data = np.array(args[1:])
-		print(self.data)
+		# [-1, 1] -> [0,1]
+		self.latent2d = (np.array(args[1:]) + 1) * 0.5
+	
+	def get_attack_handler(self,*args):
+		address = args[0]
+		self.attack = args[1]
 
 	def get_osc_coordinates(self):
-		return self.data
+		return self.latent_2d
 
 osc_handler = OSCLatent()
 dispatcher = dispatcher.Dispatcher()
-dispatcher.map("/1/xy1",osc_handler.get_coordinates_handler)
+dispatcher.map(LATENT_2D_ADDR,osc_handler.get_coordinates_handler)
+dispatcher.map(ATTACK_ADDR,osc_handler.get_attack_handler)
+
 
 
 async def main_loop():
@@ -39,6 +54,3 @@ if __name__ == "__main__":
 		await main_loop()
 		transport.close()
 	asyncio.run(init_osc_latent())
-
-# dispatcher.map("/1/fader1",test)
-
